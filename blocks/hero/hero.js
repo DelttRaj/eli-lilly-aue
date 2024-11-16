ed963f9a26b47069ae4e30f994b7f91c85c59b70
DelttRaj <rroy4@deloitte.com>
2024-11-16 21:07:03 +0530

    commiting vhtml import

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,4 +1,4 @@
-import { h, render } from 'https://esm.sh/preact';
+import { h, render,vhtml } from 'https://esm.sh/preact';
 import htm from 'https://esm.sh/htm';
 
 // Initialize htm with Preact
@@ -8,6 +8,7 @@ function Hero(){
     return html`<h1>Hello Hero!</h1>`;
 }
 export default function decorate(block) {
+    console.log("vhtml presence ",vhtml);
     block.innerHTML = '';
     render(html`<${Hero} />`, block);
 }
