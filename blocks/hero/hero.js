8a2604e82763d6f74d0a9a216e16b251b6feca66
DelttRaj <rroy4@deloitte.com>
2024-11-16 21:42:32 +0530

    adding block info extractor

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,14 +1,18 @@
-import { h, render,vhtml } from 'https://esm.sh/preact';
+import { h, render } from 'https://esm.sh/preact';
 import htm from 'https://esm.sh/htm';
 
 // Initialize htm with Preact
 const html = htm.bind(h);
 
+function extractBlockInfo(block) {
+    console.log("block info ",block);
+}
+
 function Hero(){
     return html`<h1>Hello Hero!</h1>`;
 }
 export default function decorate(block) {
-    console.log("vhtml presence ",vhtml);
+  const info = extractBlockInfo(block);
     block.innerHTML = '';
     render(html`<${Hero} />`, block);
 }
