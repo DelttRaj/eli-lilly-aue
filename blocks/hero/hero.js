d8bcab85cd761d32aaedccf8f73db4d787328ce7
Raj <rroy4@deloitte.com>
2024-11-17 21:32:39 +0530

    Update hero.js with ref and additional changes

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,22 +1,34 @@
 import { html, render } from 'https://esm.sh/htm/preact/standalone';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-
 function extractBlockInfo(block) {
-    const herodataJSON = {    };
+    const herodataJSON = {};
     herodataJSON.textElement = block.querySelector("div:nth-child(2) > div > div");
     return herodataJSON;
 }
 
+function Hero(props) {
+    // Reference for the div wrapper to enable instrumentation
+    const wrapperRef = (node) => {
+        if (node && props?.data?.textElement) {
+            // Move instrumentation and keep it editable
+            moveInstrumentation(props.data.textElement, node);
+        }
+    };
 
-function Hero(props){
-    const preactHeroWrapper = document.createElement("div");
-   // const textContent = textElement.textContent;
-    moveInstrumentation(props?.data?.textElement,preactHeroWrapper)
-    return html`<div><${preactHeroWrapper.outerHTML}/></div>`;
+    // Use HTM to ensure the wrapper is included in the JSX tree
+    return html`
+        <div>
+            <div ref=${wrapperRef} data-editable="true" data-attribute="hero">
+                <!-- Preact will manage this div, but it remains editable for AEM -->
+            </div>
+        </div>
+    `;
 }
+
 export default function decorate(block) {
     const info = extractBlockInfo(block);
-  //  block.textContent = "";
-    render(html`<${Hero} data=${info}/>`, block);
+
+    // Render the Hero component into the block
+    render(html`<${Hero} data=${info} />`, block);
 }
