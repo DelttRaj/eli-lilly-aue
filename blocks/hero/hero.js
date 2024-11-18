416543399952d4a3c1bc9ef4aac538717b3c0ebd
DelttRaj <rroy4@deloitte.com>
2024-11-18 10:54:22 +0530

    changing the p to h2 and adding some comments

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,19 +1,31 @@
 import { html, render } from 'https://esm.sh/htm/preact/standalone';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
+/**
+ * This method is for extracting the block common for both live-EDS page and Editor enabled page in AEM
+ * @param {HTMLElement} block 
+ * @returns {Object} JSON - which then passed as props to Hero Component in preact
+ */
 function extractBlockInfo(block) {
     const herodataJSON = {};
  //  herodataJSON.textBlock  = block.children[1].querySelector(`[data-aue-type="richtext"]`);
-   herodataJSON.textBlock  = block.children[1].children[0];
+   herodataJSON.textBlock  = block.children[1].children[0]; 
    return herodataJSON;
 }
 
+/**
+ * This component takes props as object from the extracted block and then refines the data to be fitted and editable in both EDS live page 
+ * and in editor mode
+ * @param {Object} props 
+ * @returns {Object} 
+ */
 function Hero(props) {
     const textContent = props?.data?.textBlock.textContent;
     const wrapperRef = (node) => {
         if (node && props?.data?.textBlock) {
-            const textBlock = props?.data?.textBlock.querySelector(`[data-aue-type="richtext"]`);
-            if(textBlock){
+            const textBlock = props?.data?.textBlock.querySelector(`[data-aue-type="richtext"]`); 
+            if(textBlock)
+            {
                 moveInstrumentation(textBlock, node);
             }
         }
@@ -27,7 +39,11 @@ function Hero(props) {
         </div>
     `;
 }
-
+/**
+ * This is the default function render the block when loadBlock gets called from script.js
+ * This calls the render function from preact
+ * @param {HTMLElement} block 
+ */
 export default function decorate(block) {
     const info = extractBlockInfo(block);
     block.textContent = '';
